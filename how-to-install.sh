0c9440b99229ce7d8a3931db51e0cfefdef17370
Adriano Freitas <dev@adrianofreitas.me>
2017-01-25 00:22:34 -0200

    arquivo how-to-install.sh atualizado

--- how-to-install.sh
+++ how-to-install.sh
@@ -4,15 +4,17 @@
 #  [+] License: GPLv3
 #  [+] Version: 0.1.2
 
-#1
+#1 - Install dependencies
 apt-get install curl php5 php5-curl
-#2
+
+#2 - Create directory
 mkdir /opt/macvendor
-#3
+
+#3 - Copy to directory
 cp macvendor.php /opt/macvendor/
-#4
-mkdir /opt/macvendor
-#5
+
+#5 - Create a alias
 echo "alias macvendor='php /opt/macvendor/macvendor.php'" | tee -a ~/.bash_aliases
-#6
+
+#6 - Refresh file ~/.bashrc
 source ~/.bashrc
